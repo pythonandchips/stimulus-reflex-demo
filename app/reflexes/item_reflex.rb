f732940bb09d50acbe60629e73a996a8f8a17a29
Colin Gemmell <pythonandchips@gmail.com>
2021-05-01 16:36:48 +0100

    show modal for line items

---
+++ app/reflexes/item_reflex.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ItemReflex < ApplicationReflex
+  def show
+    invoice = Invoice.find(params[:id])
+    item = invoice.items.build
+
+    morph(
+      "#new_item_modal",
+      render(
+        partial: "items/modal",
+        locals: {
+          invoice: invoice,
+          item: item
+        }
+      )
+    )
+  end
+end
