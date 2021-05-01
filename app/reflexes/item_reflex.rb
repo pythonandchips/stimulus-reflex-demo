ff308de0f2b4610463a026329af63be1dd09f004
Colin Gemmell <pythonandchips@gmail.com>
2021-05-01 16:43:09 +0100

    create items from the modal

--- app/reflexes/item_reflex.rb
+++ app/reflexes/item_reflex.rb
@@ -17,7 +17,30 @@ class ItemReflex < ApplicationReflex
     )
   end
 
+  def create
+    invoice = Invoice.find(params[:id])
+
+    invoice.items.create!(item_params)
+
+    morph(
+      "#invoice_items",
+      render(
+        partial: "invoices/items",
+        locals: {
+          invoice: invoice
+        }
+      )
+    )
+    close
+  end
+
   def close
     morph("#new_item_modal", "")
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:description, :quantity, :price)
   end
 end
